dc073b7015beb484555b8eb5094488997e08242f
Tony Delapierre <tdelapierre@Tonys-MacBook-Pro.local>
2022-03-09 17:24:08 +0100

    [FIX] json loader to take the id of each order as the key and the order as the value in ETS

--- chap1/step5/lib/json_loader.ex
+++ chap1/step5/lib/json_loader.ex
@@ -1,11 +1,11 @@
 defmodule JsonLoader do
-  defp insert_in_db(content, database) when is_tuple(content), do: GenServer.call(database, {:post, content})
-  defp insert_in_db(content, database) when is_map(content), do: Enum.each(content, fn elem -> insert_in_db(elem, database) end)
+#  defp insert_in_db(content, database) when is_tuple(content), do: GenServer.call(database, {:post, content})
+#  defp insert_in_db(content, database) when is_map(content), do: Enum.each(content, fn elem -> insert_in_db(elem, database) end)
 
   def load_to_database(database, json_file) do
     json_file
     |> File.read!()
     |> Poison.decode!()
-    |> Enum.each(fn elem -> insert_in_db(elem, database) end)
+    |> Enum.each(fn elem -> GenServer.call(database, {:post, {elem["id"], elem}}) end)
   end
 end
